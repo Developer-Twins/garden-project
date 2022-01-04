bc819e4cbe3c2e6d899faa7c682b57b95d08265d
Mohammad Lotfalian <mohammad.lotfaliian@gmail.com>
2022-01-04 20:14:39 +0330

    update some file and fix some image name

--- src/components/Home/TheHomeComment.vue
+++ src/components/Home/TheHomeComment.vue
@@ -1,17 +1,20 @@
 <template>
-  <section
-    class='garden-home-comment'
-    ref="comment"
-  >
-    <h4 :class="[{active: activeSubtitle },'garden-home-comment__subtitle home-subtitle']" ref="subtitle">نظرات شما</h4>
+  <section :class="[{active:activeCommentContainer},'garden-home-comment']" ref="comment">
+    <div class="garden-home-comment__bg"></div>
+    <h4
+      :class="[
+        { active: activeSubtitle },
+        'garden-home-comment__subtitle home-subtitle',
+      ]"
+      ref="subtitle"
+    >
+      نظرات شما
+    </h4>
     <h1 class="garden-home-comment__title home-title">
       مردم درباره ما چه می گویند؟
     </h1>
 
-    <div class="garden-home-comment__content">
-      
-    </div>
-    
+    <div class="garden-home-comment__content"></div>
   </section>
 </template>
 
@@ -19,35 +22,46 @@
 export default {
   data() {
     return {
+      commentContainer: null,
+      activeCommentContainer:null,
       subtitle: null,
       activeSubtitle: null,
-      datas:[
+      datas: [
         {
-          describe:" سرتاسر باغچه را به صورت مرتب و منظم حرس کردند و خاک گلدان های زینتی نیز عوض گردید عملکرد این تیم بسیار عالی بود.",
-          name:"سوشا رفیعی",
+          describe:
+            " سرتاسر باغچه را به صورت مرتب و منظم حرس کردند و خاک گلدان های زینتی نیز عوض گردید عملکرد این تیم بسیار عالی بود.",
+          name: "سوشا رفیعی",
         },
         {
-          describe:" مدت ها بود آفت به  جان درخت های باغم افتاده بود و هر ترفند سمپاشی را روی آن امتحان کردم ولی تاکنون سمپاشی با پهپاد را امتحان نکرده بودم. ",
-          name:"ستاره رحمانی",
-
+          describe:
+            " مدت ها بود آفت به  جان درخت های باغم افتاده بود و هر ترفند سمپاشی را روی آن امتحان کردم ولی تاکنون سمپاشی با پهپاد را امتحان نکرده بودم. ",
+          name: "ستاره رحمانی",
         },
         {
-          describe:"در فضای کارم سال ها بود که نیاز به یک چشم انداز سرسبز میکردم که این تیم طرح این چشم انداز را برایم به صورت عالی پیاده سازی کردند.",
-          name:"رضا خاکپور",
-        }
-      ]
+          describe:
+            "در فضای کارم سال ها بود که نیاز به یک چشم انداز سرسبز میکردم که این تیم طرح این چشم انداز را برایم به صورت عالی پیاده سازی کردند.",
+          name: "رضا خاکپور",
+        },
+      ],
     };
   },
   mounted() {
+    this.commentContainer = this.$refs.comment;
     this.subtitle = this.$refs.subtitle;
     let subtitleTopPoint = null;
+    let commentContaineTopPoint = null;
 
     window.addEventListener("scroll", () => {
-      subtitleTopPoint = this.subtitle.getBoundingClientRect()
+      subtitleTopPoint = this.subtitle.getBoundingClientRect().top;
+      commentContaineTopPoint = this.commentContainer.getBoundingClientRect()
         .top;
 
       if (subtitleTopPoint < this.$store.getters.getTriggerPoint) {
         this.activeSubtitle = true;
+      }
+
+      if (commentContaineTopPoint < this.$store.getters.getTriggerPoint) {
+        this.activeCommentContainer = true;
       }
     });
   },
