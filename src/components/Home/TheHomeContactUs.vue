f531fb71c52de4a14060115c0cad7ee52190810c
Mohammad Lotfalian <mohammad.lotfaliian@gmail.com>
2022-01-06 16:03:00 +0330

    finished contact section

--- src/components/Home/TheHomeContactUs.vue
+++ src/components/Home/TheHomeContactUs.vue
@@ -1,13 +1,44 @@
 <template>
-  f
+  <section class="garden-home-contact">
+    <div class="home-contact">
+      <div class="home-contact-text">
+        <h4 class="garden-home-contact__subtitle home-subtitle">
+          ارتباط با ما
+        </h4>
+        <h1 class="garden-home-contact__title">
+          با ما در تماس باشید
+        </h1>
+        <div class="home-contact-content">
+          <form class="home-contact-form">
+          <div class="home-contact-form__name">
+            <input type="text" placeholder="نام"/>
+            <i class="fas fa-user-circle"></i>
+          </div>
+          <div class="home-contact-form__email">
+            <input type="text" placeholder="ایمیل"/>
+            <i class="fas fa-envelope"></i>
+          </div>
+          <div class="home-contact-form__comment">
+            <textarea cols="25" rows="5" placeholder="متن"></textarea>
+            <i class="fas fa-envelope-open"></i>
+          </div>
+          <div class="home-contact-form__submit">
+            <button type="button">ارسال</button>
+          </div>
+          </form>
+        </div>
+      </div>
+      <!-- <div class="home-contact-shape">
+        <div class="home-contact-image">
+          <img src="../../../public/gardenImages/contact-man.png" alt="cntactus">
+        </div>
+      </div> -->
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
