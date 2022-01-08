a399449f6d1e375060c75ffab9f6f5ffc4789383
Hosein Lotfalian <hossein.lotfaliian@gmail.com>
2022-01-08 10:09:58 +0330

    add footer part

--- src/components/Home/TheHomeFooter.vue
+++ src/components/Home/TheHomeFooter.vue
@@ -1,7 +1,40 @@
 <template>
-    <section class="garden-home-footer">
-        <h1>add title to footer</h1>
-    </section>
+    <footer class="garden-home-footer">
+        <header class="footer-header">
+            <div class="footer-contact">
+                <h2 class="footer-contact-title footer-title">راه های ارتباطی</h2>
+                <ul class="footer-contact-list footer-list">
+                    <li class="footer-contact-list__item footer-list__item">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <p>مشهد خیابان صدف مجاور پاساژ پلاک ۲۳</p>
+                    </li>
+                    <li class="footer-contact-list__item footer-list__item">
+                    <i class="fas fa-phone"></i>
+                    <p>55673...</p>
+                    </li>
+                    <li class="footer-contact-list__item footer-list__item">
+                    <i class="fas fa-envelope"></i>
+                    <p>example...@gmail.com</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-link">
+                <h2 class="footer-link-title footer-title">صفحات</h2>
+                <ul class="footer-link-list footer-list">
+                    <li class="footer-link-list__item footer-list__item">درباره ما</li>
+                    <li class="footer-link-list__item footer-list__item">خدمات ما</li>
+                    <li class="footer-link-list__item footer-list__item">چرا ما</li>
+                    <li class="footer-link-list__item footer-list__item">کارکنان ما</li>
+                    <li class="footer-link-list__item footer-list__item">نظرات شما</li>
+                </ul>
+            </div>
+        </header>
+        <footer class="footer-footer">
+            <div class="footer-copy">
+                <p class="footer-copy__text">Copyright 2022, Garden. All Rights Reserved</p>
+            </div>
+        </footer>
+    </footer>
 </template>
 
 <script>
